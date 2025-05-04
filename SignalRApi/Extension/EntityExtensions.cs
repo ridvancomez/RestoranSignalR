95b16f7168dd69f579d1bc5d284a377a294f0545
Rıdvan Çömez <85356125+ridvancomez@users.noreply.github.com>
2025-05-04 13:18:02 +0300

    Extension Class Yazıldı ve Program class ı sadeleştirildi

---
+++ SignalRApi/Extension/EntityExtensions.cs
@@ -0,0 +1,71 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.BusinessLayer.Concrete;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.EntityFramework;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entites;
+
+namespace SignalRApi.Extension
+{
+    public static class EntityExtensions
+    {
+        public static IServiceCollection AddProjectServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAboutService, AboutManager>();
+            services.AddScoped<IAboutDal, EfAboutDal>();
+
+            services.AddScoped<IBookingService, BookingManager>();
+            services.AddScoped<IBookingDal, EfBookingDal>();
+
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<ICategoryDal, EfCategoryDal>();
+
+            services.AddScoped<IContactUsService, ContactUsManager>();
+            services.AddScoped<IContactUsDal, EfContactUsDal>();
+
+            services.AddScoped<IDiscountService, DiscountManager>();
+            services.AddScoped<IDiscountDal, EfDiscountDal>();
+
+            services.AddScoped<IFeatureService, FeatureManager>();
+            services.AddScoped<IFeatureDal, EfFeatureDal>();
+
+            services.AddScoped<IProductService, ProductManager>();
+            services.AddScoped<IProductDal, EfProductDal>();
+
+            services.AddScoped<ISocialMediaService, SocialMediaManager>();
+            services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
+
+            services.AddScoped<ITestimonialService, TestimonialManager>();
+            services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+
+            services.AddScoped<IGenericDal<About>, GenericRepository<About>>();
+            services.AddScoped<IGenericService<About>, AboutManager>();
+
+            services.AddScoped<IGenericDal<Booking>, GenericRepository<Booking>>();
+            services.AddScoped<IGenericService<Booking>, BookingManager>();
+
+            services.AddScoped<IGenericDal<Category>, GenericRepository<Category>>();
+            services.AddScoped<IGenericService<Category>, CategoryManager>();
+
+            services.AddScoped<IGenericDal<ContactUs>, GenericRepository<ContactUs>>();
+            services.AddScoped<IGenericService<ContactUs>, ContactUsManager>();
+
+            services.AddScoped<IGenericDal<Discount>, GenericRepository<Discount>>();
+            services.AddScoped<IGenericService<Discount>, DiscountManager>();
+
+            services.AddScoped<IGenericDal<Feature>, GenericRepository<Feature>>();
+            services.AddScoped<IGenericService<Feature>, FeatureManager>();
+
+            services.AddScoped<IGenericDal<Product>, GenericRepository<Product>>();
+            services.AddScoped<IGenericService<Product>, ProductManager>();
+
+            services.AddScoped<IGenericDal<SocialMedia>, GenericRepository<SocialMedia>>();
+            services.AddScoped<IGenericService<SocialMedia>, SocialMediaManager>();
+
+            services.AddScoped<IGenericDal<Testimonial>, GenericRepository<Testimonial>>();    
+            services.AddScoped<IGenericService<Testimonial>, TestimonialManager>();
+
+            return services;
+        }
+    }
+}
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -6,6 +6,7 @@ using SignalR.DataAccessLayer.Concrete;
 using SignalR.DataAccessLayer.EntityFramework;
 using SignalR.DataAccessLayer.Repositories;
 using SignalR.EntityLayer.Entites;
+using SignalRApi.Extension;
 using SignalRApi.Mapping;
 using System;
 using System.Reflection;
@@ -17,52 +18,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IAboutService, AboutManager>();
-builder.Services.AddScoped<IAboutDal, EfAboutDal>();
-
-builder.Services.AddScoped<IBookingService, BookingManager>();
-builder.Services.AddScoped<IBookingDal, EfBookingDal>();
-
-builder.Services.AddScoped<ICategoryService, CategoryManager>();
-builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
-
-builder.Services.AddScoped<IContactUsService, ContactUsManager>();
-builder.Services.AddScoped<IContactUsDal, EfContactUsDal>();
-
-builder.Services.AddScoped<IDiscountService, DiscountManager>();
-builder.Services.AddScoped<IDiscountDal, EfDiscountDal>();
-
-builder.Services.AddScoped<IFeatureService, FeatureManager>();
-builder.Services.AddScoped<IFeatureDal, EfFeatureDal>();
-
-builder.Services.AddScoped<IProductService, ProductManager>();
-builder.Services.AddScoped<IProductDal, EfProductDal>();
-
-builder.Services.AddScoped<ISocialMediaService, SocialMediaManager>();
-builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
-
-builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
-builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
-
-builder.Services.AddScoped<IGenericDal<About>, GenericRepository<About>>();
-builder.Services.AddScoped<IGenericDal<Booking>, GenericRepository<Booking>>();
-builder.Services.AddScoped<IGenericDal<Category>, GenericRepository<Category>>();
-builder.Services.AddScoped<IGenericDal<ContactUs>, GenericRepository<ContactUs>>();
-builder.Services.AddScoped<IGenericDal<Discount>, GenericRepository<Discount>>();
-builder.Services.AddScoped<IGenericDal<Feature>, GenericRepository<Feature>>();
-builder.Services.AddScoped<IGenericDal<Product>, GenericRepository<Product>>();
-builder.Services.AddScoped<IGenericDal<SocialMedia>, GenericRepository<SocialMedia>>();
-builder.Services.AddScoped<IGenericDal<Testimonial>, GenericRepository<Testimonial>>();
-
-builder.Services.AddScoped<IGenericService<About>, AboutManager>();
-builder.Services.AddScoped<IGenericService<Booking>, BookingManager>();
-builder.Services.AddScoped<IGenericService<Category>, CategoryManager>();
-builder.Services.AddScoped<IGenericService<ContactUs>, ContactUsManager>();
-builder.Services.AddScoped<IGenericService<Discount>, DiscountManager>();
-builder.Services.AddScoped<IGenericService<Feature>, FeatureManager>();
-builder.Services.AddScoped<IGenericService<Product>, ProductManager>();
-builder.Services.AddScoped<IGenericService<SocialMedia>, SocialMediaManager>();
-builder.Services.AddScoped<IGenericService<Testimonial>, TestimonialManager>();
+builder.Services.AddProjectServices();
 
 //builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
